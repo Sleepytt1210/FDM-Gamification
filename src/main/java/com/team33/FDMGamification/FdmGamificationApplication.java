56b1cf3704db8fef6143913b1acbcbe0ee4aa465
Dylon Wong <b8065261@newcastle.ac.uk>
2020-12-12 00:20:39 +0800

    Adds and implements tunnel dependency

--- src/main/java/com/team33/FDMGamification/FdmGamificationApplication.java
+++ src/main/java/com/team33/FDMGamification/FdmGamificationApplication.java
@@ -1,13 +1,29 @@
 package com.team33.FDMGamification;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataAccessException;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @SpringBootApplication
 public class FdmGamificationApplication {
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FdmGamificationApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+
+		};
+	}
 }
