27721eb089675659d3ddffc668255b5b919c3b4b
Ben Middleton (UG) <b9026218@newcastle.ac.uk>
2020-12-11 10:28:00 +0000

    Initial Commit v2

---
+++ src/main/java/com/team33/FDMGamification/FdmGamificationApplication.java
@@ -0,0 +1,13 @@
+package com.team33.FDMGamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdmGamificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdmGamificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team33/FDMGamification/FdmGamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team33.FDMGamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdmGamificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
