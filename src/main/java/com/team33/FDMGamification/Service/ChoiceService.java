3591d141cd017794792007e359094c7dd66b2a4f
Dylon Wong <b8065261@newcastle.ac.uk>
2020-12-12 02:51:24 +0800

    Created Services to perform CRUD operations on Challenge, Choice and Question entities.

---
+++ src/main/java/com/team33/FDMGamification/Service/ChoiceService.java
@@ -0,0 +1,62 @@
+package com.team33.FDMGamification.Service;
+
+import com.team33.FDMGamification.DAO.ChoiceRepository;
+import com.team33.FDMGamification.DAO.QuestionRepository;
+import com.team33.FDMGamification.Model.Question;
+import com.team33.FDMGamification.Model.Choice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ChoiceService {
+
+    @Autowired
+    private ChoiceRepository choiceRepo;
+
+    @Autowired
+    private QuestionService qts;
+
+    private static final Logger log = LoggerFactory.getLogger(ChoiceService.class);
+
+    public Choice create(String choiceText, Integer weight, Integer questionId) {
+        Question question = qts.findById(questionId);
+        Choice choice = new Choice(choiceText, weight);
+        choice.setQuestion(question);
+        return choiceRepo.saveAndFlush(choice);
+    }
+
+    public List<Choice> getAll(){
+        return choiceRepo.findAll();
+    }
+
+    public Choice update(Integer choiceId, String choiceText, Integer weight) {
+        Choice choice = findById(choiceId);
+        if(choiceText != null) choice.setChoiceText(choiceText);
+        if(weight != null) choice.setWeight(weight);
+        return choiceRepo.saveAndFlush(choice);
+    }
+
+    public void delete(Integer choiceId) {
+        choiceRepo.deleteById(choiceId);
+    }
+
+    public void delete(Choice choice) {
+        choiceRepo.delete(choice);
+    }
+
+    public void batchDelete(List<Choice> choices) {
+        choiceRepo.deleteInBatch(choices);
+    }
+
+    public Choice findById(Integer choiceId) {
+        return choiceRepo.findById(choiceId).orElseThrow(() -> new EntityNotFoundException("Choice not found!"));
+    }
+
+}
---
+++ src/main/java/com/team33/FDMGamification/Service/QuestionService.java
@@ -0,0 +1,70 @@
+package com.team33.FDMGamification.Service;
+
+import com.team33.FDMGamification.DAO.QuestionRepository;
+import com.team33.FDMGamification.Model.Challenge;
+import com.team33.FDMGamification.Model.Choice;
+import com.team33.FDMGamification.Model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepo;
+
+    @Autowired
+    private ChallengeService cls;
+
+    private static final Logger log = LoggerFactory.getLogger(QuestionService.class);
+
+    public Question create(String questionText, Integer challengeId) {
+        Challenge challenge = cls.findById(challengeId);
+        Question question = new Question(questionText);
+        question.setChallenge(challenge);
+        return questionRepo.saveAndFlush(question);
+    }
+
+    public List<Question> getAll(){
+        return questionRepo.findAll();
+    }
+
+    public Question update(Integer questionId, String questionText) {
+        Question question = findById(questionId);
+        if(questionText != null) question.setQuestionText(questionText);
+        return questionRepo.saveAndFlush(question);
+    }
+
+    public void delete(Integer questionId) {
+        questionRepo.deleteById(questionId);
+    }
+
+    public void delete(Question question) {
+        questionRepo.delete(question);
+    }
+
+    public void batchDelete(List<Question> questions) {
+        questionRepo.deleteInBatch(questions);
+    }
+
+    public Question findById(Integer questionId) {
+        return questionRepo.findById(questionId).orElseThrow(() -> new EntityNotFoundException("Challenge not found!"));
+    }
+
+    public void addChoices(Integer questionId, Choice choice) throws InstanceAlreadyExistsException{
+        Question question = findById(questionId);
+        Map<Integer, Choice> choices = question.getChoices();
+        if(choices.put(choice.getId(), choice) != null) {
+            throw new InstanceAlreadyExistsException("The choice " + choice.getChoiceText() +" already exists in question " + questionId + "!");
+        }
+        questionRepo.save(question);
+    }
+}
