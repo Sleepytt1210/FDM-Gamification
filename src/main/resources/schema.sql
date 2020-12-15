f53c663708faf91fe72f51fcc17965832c2956a4
Dylon Wong <b8065261@newcastle.ac.uk>
2020-12-15 19:56:43 +0800

    Set up admin details and models for admin page login authentication

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS Question;
 
 DROP TABLE IF EXISTS Challenge;
 
+DROP TABLE IF EXISTS Admin;
+
 CREATE TABLE Challenge(
 	scenario_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	introduction TEXT NOT NULL,
@@ -24,3 +26,12 @@ CREATE TABLE Choice(
 	question_id INT NOT NULL,
 	FOREIGN KEY (question_id) REFERENCES Question(question_id) ON DELETE CASCADE
 );
+
+CREATE TABLE Admin(
+    admin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    password VARCHAR(100) NOT NULL
+)
