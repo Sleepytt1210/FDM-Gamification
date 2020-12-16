d2e03730702d2e4cd591afaaa787a7c5880fc750
Dylon Wong <b8065261@newcastle.ac.uk>
2020-12-16 19:18:39 +0800

    Remove duplicates JdbcConfig and SSHTunnel

---
+++ src/main/java/com/team33/FDMGamification/Service/AdminDetailsService.java
@@ -0,0 +1,35 @@
+package com.team33.FDMGamification.Service;
+
+import com.team33.FDMGamification.DAO.AdminRepository;
+import com.team33.FDMGamification.Model.Admin;
+import com.team33.FDMGamification.Model.AdminDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AdminRepository adminRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Admin createUser(String username, String firstname, String lastname, String password, String phoneNo) {
+        Admin admin = new Admin(username, firstname, lastname, passwordEncoder.encode(password), phoneNo);
+        return adminRepo.saveAndFlush(admin);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminRepo.findByUsername(username);
+        if(admin == null || !admin.getUsername().equals(username)){
+            throw new UsernameNotFoundException("Admin with username " + username + " is not found!");
+        }
+        return new AdminDetails(admin.getUsername(), admin.getFirstname(), admin.getLastname(), admin.getPassword(), admin.getPhoneNo(), admin.getId());
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,17 +7,17 @@ DROP TABLE IF EXISTS Challenge;
 DROP TABLE IF EXISTS Admin;
 
 CREATE TABLE Challenge(
-	scenario_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	challenge_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	introduction TEXT NOT NULL,
 	completion INT NOT NULL
-);
+)AUTO_INCREMENT=1;
 
 CREATE TABLE Question(
 	question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	question_text TEXT NOT NULL,
-	scenario_id INT NOT NULL,
-	FOREIGN KEY (scenario_id) REFERENCES Challenge(scenario_id) ON DELETE CASCADE
-);
+	challenge_id INT NOT NULL,
+	FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) ON DELETE CASCADE
+)AUTO_INCREMENT=1;
 
 CREATE TABLE Choice(
 	choice_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -25,7 +25,7 @@ CREATE TABLE Choice(
 	choice_text TEXT NOT NULL,
 	question_id INT NOT NULL,
 	FOREIGN KEY (question_id) REFERENCES Question(question_id) ON DELETE CASCADE
-);
+)AUTO_INCREMENT=1;
 
 CREATE TABLE Admin(
     admin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,4 +34,4 @@ CREATE TABLE Admin(
     lastname VARCHAR(50) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     password VARCHAR(100) NOT NULL
-)
+)AUTO_INCREMENT=1;
