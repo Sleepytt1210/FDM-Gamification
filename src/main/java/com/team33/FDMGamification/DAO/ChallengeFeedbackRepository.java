8ccddd0c53567e450e333d82aef438368b126477
Dylon Wong <b8065261@newcastle.ac.uk>
2020-12-18 18:08:48 +0800

    Added repository and service for feedback model

---
+++ src/main/java/com/team33/FDMGamification/DAO/ChallengeFeedbackRepository.java
@@ -0,0 +1,14 @@
+package com.team33.FDMGamification.DAO;
+
+import com.team33.FDMGamification.Model.ChallengeFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ChallengeFeedbackRepository extends JpaRepository<ChallengeFeedback, Integer> {
+
+    @Query("SELECT f FROM ChallengeFeedback f WHERE f.challenge.id = :id AND f.positive = :positive")
+    ChallengeFeedback findByChallengeIdAndChallengeFeedbackPositive(@Param("id") Integer id, @Param("positive") boolean positive);
+
+
+}
---
+++ src/main/java/com/team33/FDMGamification/Service/ChallengeFeedbackService.java
@@ -0,0 +1,83 @@
+package com.team33.FDMGamification.Service;
+
+import com.team33.FDMGamification.DAO.ChallengeFeedbackRepository;
+import com.team33.FDMGamification.Model.Challenge;
+import com.team33.FDMGamification.Model.ChallengeFeedback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class ChallengeFeedbackService {
+
+    @Autowired
+    private ChallengeFeedbackRepository challengeFeedbackRepo;
+
+    @Autowired
+    private ChallengeService chs;
+
+    private static final Logger log = LoggerFactory.getLogger(ChallengeFeedbackService.class);
+
+    public ChallengeFeedback create(Integer challengeId, String challengeFeedbackTitle, String challengeFeedbackText) throws InstanceAlreadyExistsException {
+        Challenge challenge = chs.findById(challengeId);
+        ChallengeFeedback challengeFeedback = new ChallengeFeedback();
+        challengeFeedback.setChallenge(challenge);
+        challengeFeedback = challengeFeedbackRepo.saveAndFlush(challengeFeedback);
+        chs.addChallengeFeedback(challengeId, challengeFeedback);
+        return challengeFeedback;
+    }
+
+    public ChallengeFeedback create(Integer challengeId, ChallengeFeedback challengeFeedback) throws InstanceAlreadyExistsException {
+        Challenge challenge = chs.findById(challengeId);
+        challengeFeedback.setChallenge(challenge);
+        challengeFeedback = challengeFeedbackRepo.saveAndFlush(challengeFeedback);
+        chs.addChallengeFeedback(challengeId, challengeFeedback);
+        return challengeFeedback;
+    }
+
+    public List<ChallengeFeedback> getAll(){
+        return challengeFeedbackRepo.findAll();
+    }
+
+    public ChallengeFeedback update(Integer challengeFeedbackId, String challengeFeedbackTitle, String challengeFeedbackText) {
+        ChallengeFeedback challengeFeedback = findById(challengeFeedbackId);
+        if(challengeFeedbackText != null) challengeFeedback.setFeedback_text(challengeFeedbackText);
+        if(challengeFeedbackTitle != null) challengeFeedback.setFeedback_title(challengeFeedbackTitle);
+        return challengeFeedbackRepo.saveAndFlush(challengeFeedback);
+    }
+
+    public void delete(Integer challengeFeedbackId) {
+        ChallengeFeedback challengeFeedback = findById(challengeFeedbackId);
+        Challenge challenge = challengeFeedback.getChallenge();
+        challenge.getChallengeFeedback().remove(challengeFeedback.isPositive());
+        challengeFeedbackRepo.deleteById(challengeFeedbackId);
+    }
+
+    public void delete(ChallengeFeedback challengeFeedback) {
+        Challenge challenge = challengeFeedback.getChallenge();
+        challenge.getChallengeFeedback().remove(challengeFeedback.isPositive());
+        challengeFeedbackRepo.delete(challengeFeedback);
+    }
+
+    public void batchDelete(List<ChallengeFeedback> challengeFeedbacks) {
+        for(ChallengeFeedback challengeFeedback : challengeFeedbacks) {
+            Challenge challenge = challengeFeedback.getChallenge();
+            challenge.getChallengeFeedback().remove(challengeFeedback.isPositive());
+        }
+        challengeFeedbackRepo.deleteAll(challengeFeedbacks);
+    }
+
+    public ChallengeFeedback findById(Integer challengeFeedbackId) {
+        return challengeFeedbackRepo.findById(challengeFeedbackId).orElseThrow(() -> new EntityNotFoundException("ChallengeFeedback not found!"));
+    }
+
+    public ChallengeFeedback findByPositive(Integer challengeId, boolean positive) {
+        return challengeFeedbackRepo.findByChallengeIdAndChallengeFeedbackPositive(challengeId, positive);
+    }
+
+}
