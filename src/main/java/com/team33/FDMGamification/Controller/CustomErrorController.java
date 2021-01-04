7b9973cf1ebf24ee4589c70f8711be937e94c1d6
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-05 01:22:26 +0800

    Fix server does not include custom error message

--- src/main/java/com/team33/FDMGamification/Controller/CustomErrorController.java
+++ src/main/java/com/team33/FDMGamification/Controller/CustomErrorController.java
@@ -3,6 +3,7 @@ package com.team33.FDMGamification.Controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.RequestDispatcher;
@@ -12,30 +13,30 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+    public ModelAndView renderErrorPage(HttpServletRequest request) {
 
         ModelAndView errorPage = new ModelAndView("error");
-        String errorMsg = RequestDispatcher.ERROR_MESSAGE;
-        Object status = httpRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errorMsg = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         Integer statusCode = Integer.valueOf(status.toString());
 
-        if(errorMsg == null || errorMsg.equals("")) {
+        if(errorMsg == null || errorMsg.isBlank() || errorMsg.isEmpty()) {
             switch (statusCode) {
                 case 400: {
-                    errorMsg = "Http Error Code: 400. Bad Request";
+                    errorMsg = "Bad Request";
                     break;
                 }
                 case 401: {
-                    errorMsg = "Http Error Code: 401. Unauthorized";
+                    errorMsg = "Unauthorized";
                     break;
                 }
                 case 404: {
-                    errorMsg = "Http Error Code: 404. Resource not found";
+                    errorMsg = "Resource not found";
                     break;
                 }
                 case 500: {
-                    errorMsg = "Http Error Code: 500. Internal Server Error";
+                    errorMsg = "Internal Server Error";
                     break;
                 }
             }
