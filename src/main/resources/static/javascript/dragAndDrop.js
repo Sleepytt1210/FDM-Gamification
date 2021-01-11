8fc7d486e23dab08fac8be001275470bc4079edc
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-12 04:03:20 +0800

    Update css and url parser for drag and drop js

--- src/main/resources/static/javascript/dragAndDrop.js
+++ src/main/resources/static/javascript/dragAndDrop.js
@@ -1,13 +1,29 @@
+var local = window.localStorage;
+
 $(function() {
+
+    var url = window.location.href;
+    var urlSplit = url.match(/scenario\/(\d+)\/(\d+)/);
+    var qid = urlSplit[2];
     var btn = $("#submit");
     btn.prop("disabled", true);
     btn.css("cursor", "not-allowed");
     btn.prop("title", "Please fill in the tables first");
+    
+    btn.click(function() {
+    });
 
     $("ul.droptrue").sortable({
         connectWith: ".droptrue",
+        placeholder: "placeholder",
         dropOnEmpty: true,
+        start: function(event, ui) {
+            // Explicitly set placeholder height following the dragged elements height.
+            ui.placeholder.height(ui.item.height());
+            $(".droppable .droptrue").addClass("highlight");
+        },
         stop: function(event, ui) {
+            $(".droppable .droptrue").removeClass("highlight");
             var cur = ui.item;
             // Set request parameter of input according to table column
             cur.find("input").attr("name", cur.parent()[0].id);
