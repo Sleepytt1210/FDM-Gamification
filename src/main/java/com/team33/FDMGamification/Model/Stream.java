cfdce8f8ee979e50a5dac56573854e077e7d1628
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-17 23:18:12 +0800

    Added full name to stream and change stream none to show label: Choose a stream

--- src/main/java/com/team33/FDMGamification/Model/Stream.java
+++ src/main/java/com/team33/FDMGamification/Model/Stream.java
@@ -1,5 +1,16 @@
 package com.team33.FDMGamification.Model;
 
 public enum Stream {
-    ST, BI, TO, NONE
+    ST("Software Testing"), BI("Business Intelligence"), TO("Testing Operation"), NONE("NONE");
+
+    private final String fullName;
+
+    Stream(String fullName){
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+    
 }
