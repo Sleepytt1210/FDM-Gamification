1f49cf63d63b052e812e1d08c7c5306f6e3f4247
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-17 11:43:06 +0800

    Change footer layout

--- src/main/resources/static/javascript/imageUpload.js
+++ src/main/resources/static/javascript/imageUpload.js
@@ -1,15 +1,21 @@
 $(function () {
     function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+        if (input.files && input.files[0] && (input.files[0].type === "image/jpeg" || input.files[0].type === "image/png")  ) {
 
-            $("#preview").show();
+            const fileLimit = 256*1024; //256kiB
+            const fileLimitString = "256kiB";
+            if(input.files[0].size <= fileLimit) {
 
-            reader.onload = function (e) {
-                document.getElementById('preview').src = e.target.result;
+                var reader = new FileReader();
+                $("#preview").show();
+                reader.onload = function (e) {
+                    document.getElementById('preview').src = e.target.result;
+                }
+
+                reader.readAsDataURL(input.files[0]); // convert to base64 string
+            }else{
+                alert(`Image is too large! File size must not exceed ${fileLimitString}`)
             }
-
-            reader.readAsDataURL(input.files[0]); // convert to base64 string
         }
     }
 
