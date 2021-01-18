a7bac188b2dc3486ca9b5e1d375b84ea51f0a9c8
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-19 02:07:53 +0800

    Added all question types for selection

--- src/main/java/com/team33/FDMGamification/Model/QuestionType.java
+++ src/main/java/com/team33/FDMGamification/Model/QuestionType.java
@@ -1,5 +1,18 @@
 package com.team33.FDMGamification.Model;
 
 public enum QuestionType {
-    NONE, DRAG_DROP, MULTIPLE_CHOICE, TEXTBOX
+    NONE("None"),
+    DRAG_DROP("Drag and Drop"),
+    MULTIPLE_CHOICE("Multiple choices"),
+    TEXTBOX("Text box");
+
+    private final String fullName;
+
+    QuestionType(String fullname) {
+        this.fullName = fullname;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
