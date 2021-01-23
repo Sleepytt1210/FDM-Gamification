377a7b9f2574c0b4494985eeb9949c17f7f28978
Dylon Wong <b8065261@newcastle.ac.uk>
2021-01-24 01:42:36 +0800

    Added transactional read-only for optimisation

--- src/main/java/com/team33/FDMGamification/DAO/RatingRepository.java
+++ src/main/java/com/team33/FDMGamification/DAO/RatingRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional
+@org.springframework.transaction.annotation.Transactional(readOnly = true)
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
     @Query("SELECT r FROM Rating r WHERE r.challenge.id = :challengeId ")
